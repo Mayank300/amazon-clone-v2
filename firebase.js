445f3c1cfd133802cb7f3416530d624e30dcd9d3
Mayank <70268410+Mayank300@users.noreply.github.com>
2021-05-22 20:41:31 +0530

    DAY-2

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firbase from "firbase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWGWvV4B-OgX-tkz6GxwYUjDzp_1YXEr8",
+  authDomain: "clone-v2-82373.firebaseapp.com",
+  projectId: "clone-v2-82373",
+  storageBucket: "clone-v2-82373.appspot.com",
+  messagingSenderId: "752833000287",
+  appId: "1:752833000287:web:a8cd052873d32ec0d0fcea",
+};
